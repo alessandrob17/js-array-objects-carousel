2a3839bcab42e59b3131e50a561d5b880410c3d9
alessandrob17 <a.baldipardi@gmail.com>
2023-02-15 16:12:06 +0100

    FEAT carosello

---
+++ app.js
@@ -0,0 +1,25 @@
+// Array
+
+let slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+]
+
+
+let indiceSlide = 0
+const slideElements = document.getElementsByClassName('slide')
+console.log(slideElements)
+
+
+
+
+
+
+
+// BOTTONI
+
+let leftBtn = document.querySelector('arrow-left')
+let rightBtn = document.querySelector('arrow-right')
