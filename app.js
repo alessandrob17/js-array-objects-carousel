326e1606d3d14bc6bb765ed9d646cb6ce7cdbc8a
alessandrob17 <a.baldipardi@gmail.com>
2023-02-20 13:06:32 +0100

    FEAT carousel

--- app.js
+++ app.js
@@ -1,17 +1,73 @@
-// Array
+// Array oggetto
 
-let slides = [
-	'./img/01.jpg',
-	'./img/02.jpg',
-	'./img/03.jpg',
-	'./img/04.jpg',
-	'./img/05.jpg',
-]
+const images = [
+    {
+        image: './img/01.webp',
+        title: 'Marvel\\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: './img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: './img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: './img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: './img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+const carousel = document.querySelector('.carousel');
+const slides = carousel.querySelectorAll('.slide');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+
+let currentSlide = 0;
+
+arrowLeft.addEventListener('click', () => {
+  if (currentSlide > 0) {
+    currentSlide--;
+  } else {
+    currentSlide = slides.length - 1;
+  }
+  updateCarousel();
+});
+
+arrowRight.addEventListener('click', () => {
+  if (currentSlide < slides.length - 1) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
+  }
+  updateCarousel();
+});
+
+function updateCarousel() {
+  slides.forEach((slide, index) => {
+    if (index === currentSlide) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
 
 
-let indiceSlide = 0
-const slideElements = document.getElementsByClassName('slide')
-console.log(slideElements)
+
+
+
+
+
+//  let indiceSlide = 0
+//  const slideElements = document.getElementsByClassName('slide')
+//  console.log(slideElements)
 
 
 
@@ -19,7 +75,7 @@ console.log(slideElements)
 
 
 
-// BOTTONI
+// // // BOTTONI
 
-let leftBtn = document.querySelector('arrow-left')
-let rightBtn = document.querySelector('arrow-right')
+//  let leftBtn = document.querySelector('arrow-left')
+// let rightBtn = document.querySelector('arrow-right')
